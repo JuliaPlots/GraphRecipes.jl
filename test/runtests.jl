e4a06f7e881440b2b8979dd53c11abc1e8c4583f
Thomas Breloff <tom@breloff.com>
2016-06-14 13:44:14 -0400

    commented out tests; updated travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,25 +9,25 @@ using Base.Test
 # -----------------------------------------
 # marginalhist
 
-using Distributions
-n = 1000
-x = rand(Gamma(2), n)
-y = -0.5x + randn(n)
-marginalhist(x, y)
+# using Distributions
+# n = 1000
+# x = rand(Gamma(2), n)
+# y = -0.5x + randn(n)
+# marginalhist(x, y)
 
 # -----------------------------------------
 # portfolio composition map
 
-# fake data
-tickers = ["IBM", "Google", "Apple", "Intel"]
-N = 10
-D = length(tickers)
-weights = rand(N,D)
-weights ./= sum(weights, 2)
-returns = sort!((1:N) + D*randn(N))
+# # fake data
+# tickers = ["IBM", "Google", "Apple", "Intel"]
+# N = 10
+# D = length(tickers)
+# weights = rand(N,D)
+# weights ./= sum(weights, 2)
+# returns = sort!((1:N) + D*randn(N))
 
-# plot it
-portfoliocomposition(weights, returns, labels = tickers')
+# # plot it
+# portfoliocomposition(weights, returns, labels = tickers')
 
 # -----------------------------------------
 # 
